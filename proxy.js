95dc1c8277d44d048a97526331ae193dd9ab12f9
sfdd <sfdd@sfdddeiMac.local>
2017-04-14 10:04:43 +0800

    first commit

---
+++ proxy.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const proxy = require('http-proxy-middleware')
+const args = require('node-args')
+
+const { proxyList, proxyListLocal } = require('./config')
+
+const isRemote = !args.local
+const isLocal = args.local
+
+const app = express()
+
+if (isRemote) {
+  proxyList.forEach(item => {
+    app.use(
+      proxy(item.from, {
+        target: item.target,
+        changeOrigin: true
+      })
+    )
+  })
+}
+
+if (isLocal) {
+  proxyListLocal.forEach(item => {
+    app.use(
+      proxy(item.from, {
+        target: item.target,
+        changeOrigin: true
+      })
+    )
+  })
+}
+
+console.log('请注意使用sudo 执行在linux下的80端口权限！')
+app.listen(80, '0.0.0.0', () => {
+  console.log('proxy server is running on 80!')
+})
