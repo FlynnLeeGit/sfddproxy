09cadcca90cb747ab069c7c648767d1b1289a094
sfdd <sfdd@sfdddeiMac.local>
2017-04-17 09:41:21 +0800

    update config

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ const isFront = pathname =>
   !(pathname.match('/_fapi') ||
     pathname.match('/_common') ||
     pathname.match('/_bapi') ||
-    pathname.match('/superman'))
+    pathname.match('/superman') ||
+    pathname.match('/virtual_reality'))
 
 const frontHmrFilter = (pathname, req) => {
   const isFrontHmr = pathname.match('/__webpack_hmr') &&
@@ -18,29 +19,41 @@ const backendHmrFilter = (pathname, req) => {
   return isBackendHmr
 }
 
+const vrHmrFilter = (pathname, req) => {
+  const isVrHmr = pathname.match('/__webpack_hmr') &&
+    req.headers.referer.match('/virtual_reality')
+  return isVrHmr
+}
+
 const frontFilter = (pathname, req) => {
   console.log('请求Path', pathname, isFront(pathname))
+  console.log('请求referer',req.headers.referer)
   return isFront(pathname)
 }
+
 module.exports = {
   proxyList: [
     { from: '/_fapi', target: 'http://develop.sfdd.lab' },
     { from: '/_bapi', target: 'http://develop.sfdd.lab' },
     { from: '/_common', target: 'http://develop.sfdd.lab' },
-    { from: '/superman', target: 'http://localhost:3000' },
-    { from: backendHmrFilter, target: 'http://localhost:3000' },
-    { from: frontHmrFilter, target: 'http://localhost:8080' },
+    { from: '/superman', target: 'http://localhost:5188' },
+    { from: '/virtual_reality', target: 'http://localhost:5000' },
+    { from: backendHmrFilter, target: 'http://localhost:5188' },
+    { from: vrHmrFilter, target: 'http://localhost:5000' },
+    { from: frontHmrFilter, target: 'http://localhost:5180' },
     // 此条规则一定要放在最后来匹配前台路径代理
-    { from: frontFilter, target: 'http://localhost:8080' }
+    { from: frontFilter, target: 'http://localhost:5180' }
   ],
   proxyListLocal: [
     { from: '/_fapi', target: 'http://localhost:8000' },
     { from: '/_bapi', target: 'http://localhost:8000' },
     { from: '/_common', target: 'http://localhost:8000' },
-    { from: '/superman', target: 'http://localhost:3000' },
-    { from: backendHmrFilter, target: 'http://localhost:3000' },
-    { from: frontHmrFilter, target: 'http://localhost:8080' },
+    { from: '/superman', target: 'http://localhost:5188' },
+    { from: '/virtual_reality', target: 'http://localhost:5000' },
+    { from: backendHmrFilter, target: 'http://localhost:5188' },
+    { from: vrHmrFilter, target: 'http://localhost:5000' },
+    { from: frontHmrFilter, target: 'http://localhost:5180' },
     // 此条规则一定要放在最后来匹配前台路径代理
-    { from: frontFilter, target: 'http://localhost:8080' }
+    { from: frontFilter, target: 'http://localhost:5180' }
   ]
 }
